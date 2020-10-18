43a296f8136af2ba6d422d3d8dd43a8e55c6f6a6
Utkarsh dubey <dubeyutkarsh2000@gmail.com>
2020-10-18 21:13:30 +0530

    Create C-ABBB.py

---
+++ C-ABBB.py
@@ -0,0 +1,57 @@
+
+
+#https://codeforces.com/contest/1428/problem/C
+
+from math import *
+from collections import deque
+from copy import deepcopy
+import sys
+def inp(): return sys.stdin.readline().rstrip("\r\n") #for fast input
+def multi(): return map(int,input().split())
+def strmulti(): return map(str, inp().split())
+def lis(): return list(map(int, inp().split()))
+def lcm(a,b): return (a*b)//gcd(a,b)
+def ncr(n,r): return factorial(n) // (factorial(r) * factorial(max(n - r, 1)))
+def stringlis(): return list(map(str, inp().split()))
+def out(var): sys.stdout.write(str(var))  #for fast output, always take string
+def printlist(a) :
+    print(' '.join(str(a[i]) for i in range(len(a))))
+
+def isPrime(n) :
+    if (n <= 1) : return False
+    if (n <= 3) : return True
+    if (n % 2 == 0 or n % 3 == 0) : return False
+    i = 5
+    while(i * i <= n) :
+        if (n % i == 0 or n % (i + 2) == 0) :
+            return False
+        i = i + 6
+    return True
+
+#copied functions end
+
+#start coding
+
+t=int(input())
+
+for _ in range(t):
+    s=inp()
+    a=deque([])
+    for i in s:
+        if i=='A':
+            a.append(i)
+        elif len(a)!=0:
+            a.pop()
+        else:
+            a.append(i)
+
+    print(len(a))
+
+
+
+
+
+
+
+
+
